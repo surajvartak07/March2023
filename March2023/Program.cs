7c6cc853aa71eeff657d4a82b2d3d76b1fb07997
Suraj Vartak <surajvartak07@gmail.com>
2023-04-10 22:40:15 +1000

    added comments

--- March2023/Program.cs
+++ March2023/Program.cs
@@ -33,40 +33,50 @@ else
 
 //Create new Time entry  
 
+//Find and click on create new button
 IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
 createNewButton.Click();
 
+//click on dropdown 
 IWebElement tmDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[1]"));
 tmDropdown.Click();
 
 Thread.Sleep(2000);
 
+//click on time option from dropdown 
 IWebElement timeOption = driver.FindElement(By.XPath("//*[@id=\"TypeCode_listbox\"]/li[2]"));
 timeOption.Click();
 
+//enter text in code textbox
 IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
 codeTextbox.SendKeys("TimeTest");
 
+//enter text in description textbox
 IWebElement descriptionText = driver.FindElement(By.Id("Description"));
 descriptionText.SendKeys("DescriptionTest");
 
+//click on price textbox to select it for entering the text because this textbox accepts value only after clicking on it once
 IWebElement priceOverlapBox = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
 priceOverlapBox.Click();
-
+//enter value in price tyextbox 
 IWebElement priceText = driver.FindElement(By.Id("Price"));
 priceText.SendKeys("100");
+
 
 IWebElement saveButtion = driver.FindElement(By.Id("SaveButton"));
 saveButtion.Click();
 
 Thread.Sleep(2000);
 
+//go to last page because newly added entry goes to last page last row
 IWebElement goToLastPage = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
 goToLastPage.Click();
 Thread.Sleep(2000);
 
+//changed tr[4] part in xpath to tr[last()] so that it always finds last entry
 IWebElement latestCreated = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
 
+//checking if newly created entry exists
 if (latestCreated.Text == "TimeTest")
 {
     Console.WriteLine("Created Successfully");
@@ -86,6 +96,7 @@ IWebElement editDropdownValue = driver.FindElement(By.XPath("//*[@id=\"TimeMater
 editDropdownValue.Click();
 Thread.Sleep(2000);
 
+//editing dropdown value from time to material
 IWebElement materialOption = driver.FindElement(By.XPath("//*[@id=\"TypeCode_listbox\"]/li[1]"));
 materialOption.Click();
 
@@ -117,15 +128,18 @@ else
 }
 
 //Delete newly created record
+Thread.Sleep(2000);
 
 IWebElement deleteButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[2]"));
 deleteButton.Click();
 
+//clicking OK on popup window
 driver.SwitchTo().Alert().Accept();
 Thread.Sleep(5000);
 
-IWebElement deletedRecord = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
-if (deletedRecord.Text == "MaterialTestEdited")
+//last record should not be 'MaterialTestEdited' because we deleted that record 
+IWebElement lastRecord = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+if (lastRecord.Text == "MaterialTestEdited")
 {
     Console.WriteLine("Could not delete");
 }
